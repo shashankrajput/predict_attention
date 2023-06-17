a3ffc822b772119859e4ccd4bfb269276b0036fc
user1 <rajput.shashank11@gmail.com>
2023-06-17 00:00:06 -0500

    learn conv

--- bucketing.py
+++ bucketing.py
@@ -4,54 +4,76 @@ import math
 import pickle
 
 att_list = torch.load('att_openweb_small.pt')
+# att = torch.stack(att_list)
+conv = torch.nn.Conv1d(in_channels=1, out_channels=1, kernel_size=7, stride=1, bias=False, padding_mode='reflect').cuda()
+positional_bias = torch.zeros(att_list[0].shape[-1], requires_grad=True).cuda()
+seq_len = len(att_list)
+results = {}
+for i in range(seq_len):
+    print(i)
+    att = att_list[i]
+
+    att_probs = torch.nn.functional.softmax(att,dim=-1)
+
+    att_block_size = att.shape[-1]
+    att_block_size_new = math.ceil(math.sqrt(att_block_size))**2
+
+    if att_block_size_new>att_block_size:
+        att = torch.nn.functional.pad(att, (att_block_size_new-att_block_size, 0))
+    att_block_size = att_block_size_new
+
+
+    num_buckets = int(math.sqrt(att_block_size))
+    bucket_size = num_buckets
+
+    
+    att_bucketized = torch.reshape(att, att.shape[:-1]+(num_buckets, bucket_size))
+
+    att_probs_bucketized = torch.reshape(att_probs, att_probs.shape[:-1]+(num_buckets, bucket_size))
+    att_probs_bucketized_norm = torch.norm(att_probs_bucketized, dim=-1)
+    
+
+
+
+    layers = att.shape[1]
+    heads = att.shape[2]
+
+    
+        
+    for l in range(layers):
+        if l not in results:
+            results[l] = {}
+        for h in range(heads):
+            if h not in results[l]:
+                results[l][h]=[]
+
+            att_probs_bucketized_norm[0, l, h, 0,:3] +=1
+            att_probs_bucketized_norm[0, l, h, 0,-3:] +=1
+
+            _, ind_prev = torch.topk(att_probs_bucketized_norm[0, l, h, 0], num_buckets, dim=-1)
+
+            ind_prev_top = ind_prev[:num_buckets//3]
+            ind_prev_mid = ind_prev[num_buckets//3:2*num_buckets//3]
+            # ind_prev_bot = ind_prev[2*num_buckets//3:]
+            
+            approx_attn = torch.full(att_bucketized[1, l, h, 0].shape, fill_value=float('-inf')).to(torch.bfloat16).cuda()
+            approx_attn[ind_prev_top] = att_bucketized[1, l, h, 0][ind_prev_top]
+            approx_attn[ind_prev_mid] = att_bucketized[1, l, h, 1][ind_prev_mid]
+
+            
+            approx_attn_prob = torch.nn.functional.softmax(torch.flatten(approx_attn,start_dim=-2),dim=-1)
+            
+
+            error = torch.norm(att_probs[1, l, h, 0] - approx_attn_prob)
+            
+            results[l][h].append(error.item())
+
+            # err_mid = att_bucketized[1, l, h, 0][ind_prev_mid]-att_bucketized[1, l, h, 1][ind_prev_mid]
+            # err_bot = att_bucketized[1, l, h, 0][ind_prev_bot]-att_bucketized[1, l, h, 2][ind_prev_bot]
+            # results[l][h].append(torch.norm(torch.tensor([torch.norm(err_mid), torch.norm(err_bot)])).item()/ torch.norm(att_bucketized[1, l, h, 0]).item())
+
+
 breakpoint()
-att = torch.stack(att_list)
-
-
-
-att_block_size = att.shape[-1]
-att_block_size_new = math.ceil(math.sqrt(att_block_size))**2
-
-if att_block_size_new>att_block_size:
-    att = torch.nn.functional.pad(att, (att_block_size_new-att_block_size, 0))
-att_block_size = att_block_size_new
-
-
-num_buckets = int(math.sqrt(att_block_size))
-bucket_size = num_buckets
-
-att_bucketized = torch.reshape(att, att.shape[:-1]+(num_buckets, bucket_size))
-att_bucketized = torch.sum(att_bucketized, dim=-1)
-
-
-
-seq_len = att.shape[0]
-layers = att.shape[2]
-heads = att.shape[3]
-
-sparsity = 0.3
-
-results = []
-budget =  round(sparsity*num_buckets)
-fixed_window = 3
-budget -= 2*fixed_window
-for l in range(layers):
-    print(l)
-    results.append([])
-    for h in range(heads):
-        results[l].append([])
-        for i in range(0, seq_len):
-            
-            att_bucketized[i, 0, l, h,:fixed_window]+=1
-            att_bucketized[i, 0, l, h,-fixed_window:]+=1
-            
-            _, ind_curr = torch.topk(att_bucketized[i, 1, l, h],budget, dim=-1)
-            _, ind_prev = torch.topk(att_bucketized[i, 0, l, h], budget, dim=-1)
-            
-            att_bucketized[i, 0, l, h,:4]-=1
-            att_bucketized[i, 0, l, h,-4:]-=1
-
-            results[l][h].append(torch.sum(att_bucketized[i, 1, l, h][ind_prev]).item())
             
 
 
---
+++ learn_conv.py
@@ -0,0 +1,95 @@
+import torch
+import numpy as np
+import math
+import pickle
+
+att_list = torch.load('att_openweb_small.pt')
+# att = torch.stack(att_list)
+
+seq_len = len(att_list)
+
+window = 30
+
+class Conv_Model(torch.nn.Module):
+    def __init__(self, att_block_size):
+        super(Conv_Model, self).__init__()
+        self.groups=1
+        if self.groups==1:
+            self.conv = torch.nn.Conv1d(in_channels=1, out_channels=1, kernel_size=2*window+1, stride=1, padding=window, bias=False).cuda()
+        else:
+            self.conv = torch.nn.Conv1d(in_channels=self.groups, out_channels=self.groups, kernel_size=2*window+1, stride=1, padding=window, bias=False, groups=self.groups).cuda()
+        with torch.no_grad():
+            self.conv.weight = torch.nn.Parameter(torch.zeros_like(self.conv.weight))
+        if self.groups==1:
+            self.positional_bias = torch.nn.Parameter(torch.zeros(att_block_size, requires_grad=True).cuda())
+        else:
+            self.positional_bias = torch.nn.Parameter(torch.zeros((1, self.groups, att_block_size), requires_grad=True).cuda())
+
+    def forward(self, x):
+        if self.groups==1:
+            return self.conv(x) + self.positional_bias
+        else:
+            x = torch.transpose(x, 0, 1)
+            
+            output = self.conv(x) + self.positional_bias
+
+            return torch.transpose(output, 0, 1)
+
+conv_model = Conv_Model(att_list[0].shape[-1])
+optimizer = torch.optim.SGD(conv_model.parameters(), lr=0.001)
+
+sparsity=0.3
+
+loss_history=[]
+acc_history=[]
+for epoch in range(10):
+    print(epoch)
+    for i in range(seq_len):
+        optimizer.zero_grad()
+
+        att = att_list[i].to(torch.float)
+        att = att[:,:,:,0,:]
+        att_probs = torch.nn.functional.softmax(att,dim=-1)
+        flattened = torch.reshape(att_probs, (2, -1, att_probs.shape[-1]))
+        flattened = torch.transpose(flattened, 0, 1)
+        
+        pred = torch.squeeze(conv_model(flattened[:,:1,:]))
+        true = flattened[:,1,:]
+        
+        true_thresholds = torch.unsqueeze(torch.quantile(true, 1-sparsity, dim=-1), dim=-1)
+        pred_thresholds = torch.unsqueeze(torch.quantile(pred, 1-sparsity, dim=-1), dim=-1)
+        
+        true_thresh = torch.sigmoid(5*(true - true_thresholds))
+        pred_thresh = torch.sigmoid(5*(pred - pred_thresholds))
+        
+
+        loss = torch.mean(torch.square(torch.norm(true_thresh - pred_thresh, dim=-1))) # + torch.mean(torch.square(torch.sum(pred, dim=-1) - 1))
+        
+        _, true_top_k = torch.topk(true_thresh, k=round(sparsity*true_thresh.shape[-1]), dim=-1)
+        _, pred_top_k = torch.topk(pred_thresh, k=round(sparsity*pred_thresh.shape[-1]), dim=-1)
+        
+        
+
+        true_top_k = torch.zeros_like(true_thresh).scatter_(dim=-1, index=true_top_k, src=torch.ones_like(true_thresh))
+        pred_top_k = torch.zeros_like(pred_thresh).scatter_(dim=-1, index=pred_top_k, src=torch.ones_like(pred_thresh))
+
+        
+        acc = torch.sum(torch.abs(pred_top_k-true_top_k), dim=-1)/2
+        acc = 1- (acc/round(sparsity*true_thresh.shape[-1]))
+        acc_history.append(torch.mean(acc).item())
+        
+        loss_history.append(loss.item())
+        loss.backward()
+        optimizer.step()
+
+
+import numpy as np
+print("np.mean(acc_history[-900:])", np.mean(acc_history[-900:]))
+print("np.mean(acc_history[:900])", np.mean(acc_history[:900]))
+print("np.mean(loss_history[-900:])", np.mean(loss_history[-900:]))
+print("np.mean(loss_history[:900])", np.mean(loss_history[:900]))
+# print("conv_model.conv.weight", conv_model.conv.weight)
+# print("conv_model.positional_bias", conv_model.positional_bias)
+breakpoint()
+
+    
